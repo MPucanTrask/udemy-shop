34e146517c5756b0c76ddabb577af7b7b56a5cae
Martin Pucan <mpucan@trask.cz>
2022-02-04 12:24:38 +0100

    add router && new data && separate components && refactoring

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import React from "react";
+import {Route, Routes} from "react-router-dom";
+
 import Homepage from "./pages/Homepage/Homepage";
+import './App.css';
+
+const HatsPage = () => {
+    return (
+        <h2>Hats Page</h2>
+    )
+}
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+        <Routes>
+            <Route exact path="/" element={<Homepage />} />
+            <Route path="/hats" element={<HatsPage />} />
+        </Routes>
     </div>
   );
 }
--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -1,20 +1,30 @@
 import React from 'react';
+import {useNavigate } from 'react-router-dom'
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`}>
-        <div
-            className='background-image'
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }}
-        />
-        <div className='content'>
-            <h1 className='title'>{title.toUpperCase()}</h1>
-            <span className='subtitle'>SHOP NOW</span>
+const MenuItem = ({ title, imageUrl, size, routeName }) => {
+    let navigate = useNavigate()
+
+    function handleClick() {
+        console.log(`${routeName}`)
+        console.log(navigate(`${routeName}`))
+    }
+
+    return (
+        <div className={`${size} menu-item`} onClick={handleClick}>
+            <div
+                className='background-image'
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+            />
+            <div className='content'>
+                <h1 className='title'>{title.toUpperCase()}</h1>
+                <span className='subtitle'>SHOP NOW</span>
+            </div>
         </div>
-    </div>
-);
+    );
+}
 
 export default MenuItem;
