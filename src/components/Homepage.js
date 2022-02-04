000ecdc59747ed50a4e3f7fa7eb115862472f984
Martin Pucan <mpucan@trask.cz>
2022-02-04 09:46:14 +0100

    initial commit

---
+++ src/components/Homepage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Homepage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h2 className="title">Hats</h2>
+                    <span className="subtitle">Shop now</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default Homepage
